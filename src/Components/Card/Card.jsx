7610f8448f7181fe5492a0d731aa906b1b7f85b9
korenrolle <korenrolle21@gmail.com>
2023-01-06 12:09:35 -0800

    Finished the card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,20 @@
-import './style/card.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 function Card({ post, onLike, onComment }) {
+  const [liked, setLiked] = useState(false);
+  const [comment, setComment] = useState('');
+
+  function handleLike() {
+    setLiked(true);
+    onLike();
+  }
+
+  function handleCommentSubmit(e) {
+    e.preventDefault();
+    onComment(comment);
+    setComment('');
+  }
+
   return (
     <div className="card">
       <div className="card-header">
@@ -11,16 +24,45 @@ function Card({ post, onLike, onComment }) {
         <p>{post.description}</p>
       </div>
       <div className="card-footer">
-        <button className="btn" onClick={onLike}>
-          Like
+        <button className="btnLikes" onClick={handleLike}>
+          {liked ? (
+            <>
+              <i className="fas fa-heart mr-2"></i>
+              Liked
+            </>
+          ) : (
+            'Like'
+          )}
         </button>
-        <button
-          className="btn"
-          onClick={() => onComment('Comment')}
-        >
-          Comment
+        <form onSubmit={handleCommentSubmit}>
+          <div className="input-group mr-2">
+            <input
+              type="text"
+              className="form-control rounded-pill"
+              placeholder="Leave a comment"
+              value={comment}
+              onChange={e => setComment(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-secondary rounded-pill">
+                Post
+              </button>
+            </div>
+          </div>
+        </form>
+        <button className="btn btn-success">
+          Review
         </button>
-        <button className="btn">Review</button>
+      </div>
+      <div className="card-comments">
+        {post.comments.map(c => (
+          <div key={c} className="card-comment">
+            <p>{c}</p>
+          </div>
+        ))}
+      </div>
+      <div className="card-likes">
+        <span>{post.likes} likes</span>
       </div>
     </div>
   );
